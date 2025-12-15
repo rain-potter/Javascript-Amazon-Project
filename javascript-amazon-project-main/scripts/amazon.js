f92db8e8d91af6ff28a04827c2ba0c693b29691b
rain.potter <rain.potter.work@gmail.com>
2025-12-15 17:34:56 -0500

    Made the cart quantity interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -74,8 +74,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
             cart.push({
             productId: productId,
             quantity: 1
-        })
+        });
         }
+        let cartQuantity =  0;
+
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     })
 })
 
